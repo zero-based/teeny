2967c4a349b178b2ab208f0fca5831061f552562
Michael Safwat Hanna <michaelsafwat.hanna@gmail.com>
2020-11-30 19:15:03 +0200

    Add Scanner Skeleton (#1)
    Co-authored-by: Micheline Medhat <MichelineMedhat@gmail.com>
    Co-authored-by: Monica Adel <monicatanios@gmail.com>
    Co-authored-by: Monica Atef <monicaatef46@gmail.com>
    Co-authored-by: Youssef Raafat <YoussefRaafatNasry@gmail.com>

--- Teeny.Core/Scanner.cs
+++ Teeny.Core/Scanner.cs
@@ -1,6 +1,13 @@
-﻿namespace Teeny.Core
+﻿using System;
+using System.Collections.Generic;
+
+namespace Teeny.Core
 {
     public class Scanner
     {
+        public List<TokenRecord> Scan(string sourceCode)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
