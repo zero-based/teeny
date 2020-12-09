1a38116239a9d12e68d1d8a88a57ca90a561864b
MichaelSafwatHanna <michaelsafwat.hanna@gmail.com>
2020-12-09 14:35:16 +0200

    Migrate UI Project to Console App

---
+++ Teeny.UI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Teeny.UI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
