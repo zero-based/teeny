af8b3dc4653be4991e14dbe0caffbf1a99e14c46
MonicaTanios <monicatanios@gmail.com>
2020-12-10 20:57:11 +0200

    Add More TestCases

--- Teeny.Core.Tests/ScannerTest.cs
+++ Teeny.Core.Tests/ScannerTest.cs
@@ -8,51 +8,49 @@ namespace Teeny.Core.Tests
     public class ScannerTest
     {
         private readonly Comparer<TokenRecord> Comparer = Comparer<TokenRecord>.Create((x, y) => (x.Token.CompareTo(y.Token) + x.Lexeme.CompareTo(y.Lexeme)));
-        Scanner scanner = new Scanner();
+        private readonly Scanner scanner = new Scanner();
 
         [TestMethod]
         public void TestValidIdentifier()
         {
             // Arrange
-            var sourceCode = "int x;";
-            var expected = new List<TokenRecord>{
+            string sourceCode = "int x;";
+            List<TokenRecord> expected = new List<TokenRecord>{
                         new TokenRecord {Lexeme = "int", Token = Token.DataTypeInt },
                         new TokenRecord {Lexeme = "x", Token= Token.Identifier },
-                        new TokenRecord {Lexeme = ";", Token= Token.SymbolSemicolon }};
+                        new TokenRecord {Lexeme = ";", Token= Token.SymbolSemicolon } };
 
             // Act
-            var actual = scanner.Scan(sourceCode);
+            List<TokenRecord> actual = scanner.Scan(sourceCode);
 
             // Assert
             CollectionAssert.AreEqual(expected, actual, Comparer);
-
         }
 
         [TestMethod]
         public void TestInvalidIdentifier()
         {
             // Arrange
-            var sourceCode = "int 2x;";
+            string sourceCode = "int 2x;";
 
             // Act
             Assert.ThrowsException<UnknownLexemeException>(() => scanner.Scan(sourceCode));
-
         }
 
         [TestMethod]
         public void TestAssignment()
         {
             // Arrange
-            var sourceCode = "int x:= 3;";
-            var expected = new List<TokenRecord>{
-                        new TokenRecord {Lexeme = "int", Token = Token.DataTypeInt },
-                        new TokenRecord {Lexeme = "x", Token= Token.Identifier },
-                        new TokenRecord {Lexeme = ":=", Token= Token.OperatorAssignment },
-                        new TokenRecord {Lexeme = "3", Token= Token.ConstantNumber },
-                        new TokenRecord {Lexeme = ";", Token= Token.SymbolSemicolon }};
+            string sourceCode = "int x:= 3;";
+            List<TokenRecord> expected = new List<TokenRecord>{
+                new TokenRecord {Lexeme = "int", Token = Token.DataTypeInt },
+                new TokenRecord {Lexeme = "x", Token= Token.Identifier },
+                new TokenRecord {Lexeme = ":=", Token= Token.OperatorAssignment },
+                new TokenRecord {Lexeme = "3", Token= Token.ConstantNumber },
+                new TokenRecord {Lexeme = ";", Token= Token.SymbolSemicolon }};
 
             // Act
-            var actual = scanner.Scan(sourceCode);
+            List<TokenRecord> actual = scanner.Scan(sourceCode);
 
             // Assert
             CollectionAssert.AreEqual(expected, actual, Comparer);
@@ -63,55 +61,168 @@ namespace Teeny.Core.Tests
         public void TestStringComment()
         {
             // Arrange
-            var sourceCode = "\"/*Not A comment*/\"";
-            var expected = new List<TokenRecord>{
+            string sourceCode = "\"/*Not A comment*/\"";
+            List<TokenRecord> expected = new List<TokenRecord>{
                         new TokenRecord {Lexeme = "\"/*Not A comment*/\"", Token= Token.ConstantString }};
 
             // Act
-            var actual = scanner.Scan(sourceCode);
+            List<TokenRecord> actual = scanner.Scan(sourceCode);
 
             // Assert
             CollectionAssert.AreEqual(expected, actual, Comparer);
-
         }
-
 
         [TestMethod]
         public void TestConstantNumber()
         {
             // Arrange
-            var sourceCode = "333";
-            var expected = new List<TokenRecord>{
+            string sourceCode = "333";
+            List<TokenRecord> expected = new List<TokenRecord>{
                         new TokenRecord {Lexeme = "333", Token= Token.ConstantNumber }};
 
             // Act
-            var actual = scanner.Scan(sourceCode);
+            List<TokenRecord> actual = scanner.Scan(sourceCode);
 
             // Assert
             CollectionAssert.AreEqual(expected, actual, Comparer);
+        }
 
+        [TestMethod]
+        public void TestInvalidConstantNumber1()
+        {
+            // Arrange
+            string sourceCode = "3E9";
+
+            // Act
+            Assert.ThrowsException<UnknownLexemeException>(() => scanner.Scan(sourceCode));
+        }
+
+        [TestMethod]
+        public void TestInvalidConstantNumber2()
+        {
+            // Arrange
+            string sourceCode = "0.";
+
+            // Act
+            Assert.ThrowsException<UnknownLexemeException>(() => scanner.Scan(sourceCode));
+        }
+
+        [TestMethod]
+        public void TestInvalidConstantNumber3()
+        {
+            // Arrange
+            string sourceCode = "0.x";
+
+            // Act
+            Assert.ThrowsException<UnknownLexemeException>(() => scanner.Scan(sourceCode));
+        }
+
+        [TestMethod]
+        public void TestInvalidConstantNumber4()
+        {
+            // Arrange
+            string sourceCode = "0.0.0";
+
+            // Act
+            Assert.ThrowsException<UnknownLexemeException>(() => scanner.Scan(sourceCode));
         }
 
         [TestMethod]
         public void TestUnclosedString()
         {
             // Arrange
-            var sourceCode = "\"foo";
+            string sourceCode = "\"foo";
 
             // Act
             Assert.ThrowsException<UnclosedStringException>(() => scanner.Scan(sourceCode));
-
         }
 
         [TestMethod]
         public void TestUnclosedComment()
         {
             // Arrange
-            var sourceCode = "/*";
+            string sourceCode = "/*";
 
             // Act
             Assert.ThrowsException<UnclosedCommentException>(() => scanner.Scan(sourceCode));
+        }
 
+        [TestMethod]
+        public void CompleteSampleTest()
+        {
+            // Arrange
+            string sourceCode =
+                @"int main() {
+                    /* Sample program in TINY language - computes factorial */
+                    read x; /*input an integer */
+                    if 0 < x then /* don't compute if x <= 0 */
+                        fact := 1;
+                        repeat
+                        fact := fact * x;
+                        x:= x - 1
+                        until x = 0
+                        write "" The Factorial is "" ;
+                        write fact; /* output factorial of x */
+                        write endl;
+                    end
+                    return 0;
+                }";
+
+            // Act
+            List<TokenRecord> expected = new List<TokenRecord> {
+                new TokenRecord {Lexeme = "int", Token= Token.DataTypeInt},
+                new TokenRecord {Lexeme = "main", Token= Token.Identifier},
+                new TokenRecord {Lexeme = "(", Token= Token.ParenthesisLeft},
+                new TokenRecord {Lexeme = ")", Token= Token.ParenthesisRight},
+                new TokenRecord {Lexeme = "{", Token= Token.CurlyBracketLeft},
+                new TokenRecord {Lexeme = "read", Token= Token.StatementRead},
+                new TokenRecord {Lexeme = "x", Token= Token.Identifier},
+                new TokenRecord {Lexeme = ";", Token= Token.SymbolSemicolon},
+                new TokenRecord {Lexeme = "if", Token= Token.StatementIf},
+                new TokenRecord {Lexeme = "0", Token= Token.ConstantNumber},
+                new TokenRecord {Lexeme = "<", Token= Token.OperatorConditionalLessThan},
+                new TokenRecord {Lexeme = "x", Token= Token.Identifier},
+                new TokenRecord {Lexeme = "then", Token= Token.StatementThen},
+                new TokenRecord {Lexeme = "fact", Token= Token.Identifier},
+                new TokenRecord {Lexeme = ":=", Token= Token.OperatorAssignment},
+                new TokenRecord {Lexeme = "1", Token= Token.ConstantNumber},
+                new TokenRecord {Lexeme = ";", Token= Token.SymbolSemicolon},
+                new TokenRecord {Lexeme = "repeat", Token= Token.StatementRepeat},
+                new TokenRecord {Lexeme = "fact", Token= Token.Identifier},
+                new TokenRecord {Lexeme = ":=", Token= Token.OperatorAssignment},
+                new TokenRecord {Lexeme = "fact", Token= Token.Identifier},
+                new TokenRecord {Lexeme = "*", Token= Token.OperatorArithmeticMultiply},
+                new TokenRecord {Lexeme = "x", Token= Token.Identifier},
+                new TokenRecord {Lexeme = ";", Token= Token.SymbolSemicolon},
+                new TokenRecord {Lexeme = "x", Token= Token.Identifier},
+                new TokenRecord {Lexeme = ":=", Token= Token.OperatorAssignment},
+                new TokenRecord {Lexeme = "x", Token= Token.Identifier},
+                new TokenRecord {Lexeme = "-", Token= Token.OperatorArithmeticMinus},
+                new TokenRecord {Lexeme = "1", Token= Token.ConstantNumber},
+                new TokenRecord {Lexeme = "until", Token= Token.StatementUntil},
+                new TokenRecord {Lexeme = "x", Token= Token.Identifier},
+                new TokenRecord {Lexeme = "=", Token= Token.OperatorConditionalEqual},
+                new TokenRecord {Lexeme = "0", Token= Token.ConstantNumber},
+                new TokenRecord {Lexeme = "write", Token= Token.StatementWrite},
+                new TokenRecord {Lexeme = "\" The Factorial is \"", Token= Token.ConstantString},
+                new TokenRecord {Lexeme = ";", Token= Token.SymbolSemicolon},
+                new TokenRecord {Lexeme = "write", Token= Token.StatementWrite},
+                new TokenRecord {Lexeme = "fact", Token= Token.Identifier},
+                new TokenRecord {Lexeme = ";", Token= Token.SymbolSemicolon},
+                new TokenRecord {Lexeme = "write", Token= Token.StatementWrite},
+                new TokenRecord {Lexeme = "endl", Token= Token.ConstantEndl},
+                new TokenRecord {Lexeme = ";", Token= Token.SymbolSemicolon},
+                // new TokenRecord {Lexeme = "end", Token= Token.end},
+                new TokenRecord {Lexeme = "return", Token= Token.StatementReturn},
+                new TokenRecord {Lexeme = "0", Token= Token.ConstantNumber},
+                new TokenRecord {Lexeme = ";", Token= Token.SymbolSemicolon},
+                new TokenRecord {Lexeme = "}", Token= Token.CurlyBracketRight}};
+
+            // Act
+            List<TokenRecord> actual = scanner.Scan(sourceCode);
+
+            // Assert
+            CollectionAssert.AreEqual(expected, actual, Comparer);
         }
     }
 }
