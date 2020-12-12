3d2231bd6abf7e6ab8fb9a23c285685190ee5000
Monica <44709914+MonicaAtef@users.noreply.github.com>
2020-12-12 14:19:26 +0200

    Add Console UI (#13)

--- Teeny.UI/Program.cs
+++ Teeny.UI/Program.cs
@@ -1,12 +1,64 @@
-﻿using System;
+﻿using ConsoleTables;
+using System;
+using System.Text;
+using Teeny.Core;
 
 namespace Teeny.UI
 {
-    class Program
+    internal static class Program
     {
-        static void Main(string[] args)
+        private static bool KeepReading { get; set; } = true;
+
+        static Program()
         {
-            Console.WriteLine("Hello World!");
+            Console.CancelKeyPress += (sender, eventArgs) =>
+            {
+                eventArgs.Cancel = true;
+                KeepReading = false;
+            };
+        }
+
+        private static void Main(string[] args)
+        {
+            const string banner = @"
+
+                ████████ ███████ ███████ ███    ██ ██    ██ 
+                   ██    ██      ██      ████   ██  ██  ██  
+                   ██    █████   █████   ██ ██  ██   ████   
+                   ██    ██      ██      ██  ██ ██    ██    
+                   ██    ███████ ███████ ██   ████    ██    
+
+                 a   t e e n y   t i n y   c o m p i l e r
+
+            ";
+
+            Console.WriteLine(banner);
+            Console.WriteLine("Enter tiny code (press ctrl + c to stop):");
+
+            var code = new StringBuilder();
+            while (KeepReading)
+            {
+                var line = Console.ReadLine();
+                code.Append(line);
+            }
+
+            try
+            {
+                var scanner = new Scanner();
+                var tokensTable = scanner.Scan(code.ToString());
+
+                Console.WriteLine();
+                ConsoleTable.From(tokensTable)
+                    .Write(Format.Alternative);
+            }
+            catch (Exception e)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("ERROR:");
+                Console.WriteLine(e.Message);
+                Console.ResetColor();
+            }
+
         }
     }
 }
