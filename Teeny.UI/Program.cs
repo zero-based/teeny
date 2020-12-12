1acb7024aa5947e2c9f58c5f72a1581d82ca5c47
MonicaAtef <monicaatef46@gmail.com>
2020-12-12 03:05:31 +0200

    Add Console UI

--- Teeny.UI/Program.cs
+++ Teeny.UI/Program.cs
@@ -1,12 +1,41 @@
-﻿using System;
+﻿using ConsoleTables;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Teeny.Core;
 
 namespace Teeny.UI
 {
+
     class Program
     {
+        private static bool keepRunning = true;
+         
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            StringBuilder code = new StringBuilder();
+            Console.CancelKeyPress += delegate (object sender, ConsoleCancelEventArgs e)
+            {
+                e.Cancel = true;
+                Program.keepRunning = false;
+            };
+
+            Console.WriteLine("Enter input:");
+            while (Program.keepRunning)
+            {
+                string  newline = Console.ReadLine();
+                code.Append(newline);
+            }
+            Scanner scanner = new Scanner();
+            List<TokenRecord> scanned = scanner.Scan(code.ToString());
+            Console.WriteLine('\n');
+            ConsoleTable
+                 .From<TokenRecord>(scanned)
+                 .Configure(o => o.NumberAlignment = Alignment.Right)
+                   .Write(Format.Alternative);
+
+            Console.ReadKey();    
+       
         }
     }
 }
