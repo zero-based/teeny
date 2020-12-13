909cea4406c01b2b3de74c90fbb6812ca57ca05e
Michael Safwat Hanna <michaelsafwat.hanna@gmail.com>
2020-12-14 00:06:55 +0200

    Setup CLI Args/Options (#14)

---
+++ Teeny.UI/Options.cs
@@ -0,0 +1,13 @@
+﻿using CommandLine;
+
+namespace Teeny.UI
+{
+    public class Options
+    {
+        [Option('f', "file", Required = false, HelpText = "Input file-name including path to be compiled.")]
+        public string InputFile { get; set; }
+
+        [Option('s', "silent", Required = false, HelpText = "Set output to verbose messages.")]
+        public bool Silent { get; set; }
+    }
+}
