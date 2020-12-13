d955c145ecee92120b62ed84590aa8fca098755e
MichaelSafwatHanna <michaelsafwat.hanna@gmail.com>
2020-12-14 00:04:13 +0200

    Setup CLI Args/Options

--- Teeny.UI/Program.cs
+++ Teeny.UI/Program.cs
@@ -1,23 +1,18 @@
-﻿using ConsoleTables;
+﻿using CommandLine;
+using ConsoleTables;
 using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using Teeny.Core;
 
 namespace Teeny.UI
 {
     internal static class Program
     {
-        private static bool KeepReading { get; set; } = true;
-
-        static Program()
-        {
-            Console.CancelKeyPress += (sender, eventArgs) =>
-            {
-                eventArgs.Cancel = true;
-                KeepReading = false;
-            };
-        }
-
         private static void Main(string[] args)
         {
             const string banner = @"
@@ -33,32 +28,114 @@ namespace Teeny.UI
             ";
 
             Console.WriteLine(banner);
+
+            var helpWriter = new StringWriter();
+            var parser = new Parser(with => with.HelpWriter = helpWriter);
+            parser.ParseArguments<Options>(args)
+                .WithParsed(RunWithOptions)
+                .WithNotParsed(errors => HandleParseError(errors, helpWriter));
+        }
+
+        private static void HandleParseError(IEnumerable<Error> errors, StringWriter helpWriter)
+        {
+            var enumerable = errors.ToList();
+            if (enumerable.IsVersion() || enumerable.IsHelp())
+                Console.WriteLine(helpWriter.ToString());
+            else
+                Console.Error.WriteLine(helpWriter.ToString());
+        }
+
+        private static void RunWithOptions(Options opts)
+        {
+            try
+            {
+                var code = ReadWithOptions(opts);
+                var tokensTable = ScanWithOptions(code, opts);
+                Debug.WriteLine(tokensTable);
+            }
+            catch (Exception e)
+            {
+                LogError(e.Message);
+            }
+        }
+
+        private static string ReadWithOptions(Options opts)
+        {
+            string code;
+
+            if (opts.InputFile == null)
+            {
+                code = ReadUserInput();
+            }
+            else
+            {
+                code = ReadInputFile(opts.InputFile);
+
+                if (opts.Silent) return code;
+
+                Console.WriteLine($"Tiny code from {opts.InputFile}:\n");
+                Console.WriteLine(code);
+            }
+
+            return code;
+        }
+
+        private static IEnumerable<TokenRecord> ScanWithOptions(string code, Options opts)
+        {
+            var scanner = new Scanner();
+            var tokensTable = scanner.Scan(code);
+
+            if (opts.Silent) return tokensTable;
+
+            Console.WriteLine();
+            ConsoleTable.From(tokensTable)
+                .Write(Format.Alternative);
+
+            return tokensTable;
+        }
+
+        private static string ReadUserInput()
+        {
             Console.WriteLine("Enter tiny code (press ctrl + c to stop):");
 
+            var keepReading = true;
+            Console.CancelKeyPress += (sender, eventArgs) =>
+            {
+                eventArgs.Cancel = true;
+                keepReading = false;
+            };
+
             var code = new StringBuilder();
-            while (KeepReading)
+            while (keepReading)
             {
                 var line = Console.ReadLine();
                 code.Append(line);
             }
 
+            return code.ToString();
+        }
+
+        private static string ReadInputFile(string inputFile)
+        {
+            if (!Regex.IsMatch(inputFile, @".*\.t$"))
+                throw new IOException("The file must be of type '*.t'.");
+
             try
             {
-                var scanner = new Scanner();
-                var tokensTable = scanner.Scan(code.ToString());
-
-                Console.WriteLine();
-                ConsoleTable.From(tokensTable)
-                    .Write(Format.Alternative);
+                using var sr = new StreamReader(inputFile);
+                return sr.ReadToEnd();
             }
-            catch (Exception e)
+            catch (IOException)
             {
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine("ERROR:");
-                Console.WriteLine(e.Message);
-                Console.ResetColor();
+                throw new IOException("The file could not be read or doesn't exist.");
             }
+        }
 
+        private static void LogError(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine($"ERROR: {message}");
+            Console.ResetColor();
         }
     }
 }
