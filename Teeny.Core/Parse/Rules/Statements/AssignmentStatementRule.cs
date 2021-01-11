a100f5b7d9420a157a8c2e68bc89d6b81ef7cef9
MichaelSafwatHanna <michaelsafwat.hanna@gmail.com>
2021-01-11 20:24:53 +0200

    Add Semicolon to Assignment Statement

--- Teeny.Core/Parse/Rules/Statements/AssignmentStatementRule.cs
+++ Teeny.Core/Parse/Rules/Statements/AssignmentStatementRule.cs
@@ -6,11 +6,12 @@ namespace Teeny.Core.Parse.Rules.Statements
     public class AssignmentStatementRule : StatementRule
     {
         public AssignmentStatementRule(TokenRecord identifier, TokenRecord assignmentOperator,
-            ExpressionRule expression)
+            ExpressionRule expression, TokenRecord semicolon)
         {
             Identifier.Assign(identifier);
             AssignmentOperator.Assign(assignmentOperator);
             Expression = expression;
+            Semicolon.Assign(semicolon);
         }
 
         public AssignmentStatementRule()
@@ -20,5 +21,6 @@ namespace Teeny.Core.Parse.Rules.Statements
         public TerminalNode Identifier { get; set; } = new TerminalNode(Token.Identifier);
         public TerminalNode AssignmentOperator { get; set; } = new TerminalNode(Token.Assignment);
         public ExpressionRule Expression { get; set; }
+        public TerminalNode Semicolon { get; set; } = new TerminalNode(Token.Semicolon);
     }
 }
