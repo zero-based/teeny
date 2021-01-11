40f9844490056d90c7ba4623c6d724b2c9672532
MichaelSafwatHanna <michaelsafwat.hanna@gmail.com>
2021-01-11 21:58:08 +0200

    Add Parser Structure

--- Teeny.Core/Parse/Parser.cs
+++ Teeny.Core/Parse/Parser.cs
@@ -1,15 +1,62 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using Teeny.Core.Parse.Rules;
+using Teeny.Core.Parse.Rules.Common;
 using Teeny.Core.Scan;
 
 namespace Teeny.Core.Parse
 {
     public class Parser
     {
+        private Queue<TokenRecord> _tokensQueue;
+        private TokenRecord CurrentRecord => _tokensQueue.Peek();
+        public List<string> ErrorList = new List<string>();
+
+        private TokenRecord Match(params Token[] tokens)
+        {
+            if (tokens.Contains(CurrentRecord.Token))
+            {
+                var tokenRecord = CurrentRecord;
+                _tokensQueue.Dequeue();
+                return tokenRecord;
+            }
+
+            ErrorList.Add($"Expected ${string.Join("or", tokens)} , found {CurrentRecord.Lexeme}");
+            return null;
+        }
+
+        private static T TryBuild<T>(Func<T> ruleBuilder)
+        {
+            try
+            {
+                return ruleBuilder();
+            }
+            catch (Exception)
+            {
+                return default;
+            }
+        }
+
         public BaseRule Parse(List<TokenRecord> tokens)
         {
-            return new ProgramRule();
+            _tokensQueue = new Queue<TokenRecord>(tokens);
+            try
+            {
+                return ParseProgram();
+            }
+            catch (InvalidOperationException e)
+            {
+                // End of code!
+                Console.WriteLine(e);
+                return null;
+            }
+        }
+
+        private BaseRule ParseProgram()
+        {
+            throw new NotImplementedException();
+        }
         }
     }
 }
