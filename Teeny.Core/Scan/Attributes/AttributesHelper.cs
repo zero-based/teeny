6fad5d1b4407eae506dca33421132aec3357e429
Youssef Raafat <YoussefRaafatNasry@gmail.com>
2021-01-13 08:34:26 +0200

    Refactor Lookup Tables
     * Make them static and initialize them in static constructor to
       be initialized only once for all instances.
     * Create helper method to generate the lookup tables.

--- Teeny.Core/Scan/Attributes/AttributesHelper.cs
+++ Teeny.Core/Scan/Attributes/AttributesHelper.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Teeny.Core.Scan.Attributes
 {
@@ -9,7 +11,21 @@ namespace Teeny.Core.Scan.Attributes
             var type = enumValue.GetType();
             var memInfo = type.GetMember(enumValue.ToString());
             var attributes = memInfo[0].GetCustomAttributes(typeof(T), false);
-            return attributes.Length > 0 ? (T) attributes[0] : null;
+            return attributes.Length > 0 ? (T)attributes[0] : null;
+        }
+
+        public static Dictionary<TEnum, TAttr> GetLookUpTable<TEnum, TAttr>() where TEnum : Enum where TAttr : Attribute
+        {
+            var table = new Dictionary<TEnum, TAttr>();
+            var enums = Enum.GetValues(typeof(TEnum)).Cast<TEnum>();
+
+            foreach (var e in enums)
+            {
+                var attr = e.GetAttributeOfType<TAttr>();
+                if (attr != null) table.Add(e, attr);
+            }
+
+            return table;
         }
     }
 }
