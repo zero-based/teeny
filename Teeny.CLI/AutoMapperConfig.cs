12bd1d86b513eebe8b6687bd0b201b69c53a81b9
Michael Safwat Hanna <michaelsafwat.hanna@gmail.com>
2021-01-13 02:58:29 +0200

    Add Parser to CLI (#26)

---
+++ Teeny.CLI/AutoMapperConfig.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using Teeny.Core.Parse;
+
+namespace Teeny.CLI
+{
+    public class AutoMapperConfig
+    {
+        public static Mapper Mapper
+        {
+            get
+            {
+                var config = new MapperConfiguration(Configure);
+                return new Mapper(config);
+            }
+        }
+
+        private static void Configure(IMapperConfigurationExpression cfg)
+        {
+            cfg.CreateMap<Node, TreeNode>()
+                .ForMember(dest => dest.Name, act => act.MapFrom(src => src.Name))
+                .ForMember(dest => dest.ChildNodes, act => act.MapFrom(src => src.Children));
+        }
+    }
+}
---
+++ Teeny.CLI/TreeNode.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using TreeSharp;
+
+namespace Teeny.CLI
+{
+    public class TreeNode : TreeNode<TreeNode, List<TreeNode>>
+    {
+        public string Name;
+        public override List<TreeNode> ChildNodes { get; set; }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
