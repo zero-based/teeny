91a9f0bd2b07d45edd3dbbdbe458d528a77b6d30
Michael Safwat Hanna <michaelsafwat.hanna@gmail.com>
2021-01-13 02:19:21 +0200

    Abstract BaseRule/TerminalNode (#24)
    Co-authored-by: Micheline Medhat <MichelineMedhat@gmail.com>
    Co-authored-by: Monica Adel <monicatanios@gmail.com>
    Co-authored-by: Monica Atef <monicaatef46@gmail.com>
    Co-authored-by: Youssef Raafat <YoussefRaafatNasry@gmail.com>

--- Teeny.Core/Parse/Rules/BaseRule.cs
+++ Teeny.Core/Parse/Rules/BaseRule.cs
@@ -1,6 +1,41 @@
-﻿namespace Teeny.Core.Parse.Rules
+﻿using System.Collections.Generic;
+using System.Reflection;
+
+namespace Teeny.Core.Parse.Rules
 {
-    public abstract class BaseRule
+    public abstract class BaseRule : Node
     {
+        public override string Name => GetType().Name;
+
+        public override IEnumerable<Node> Children
+        {
+            get
+            {
+                var properties = GetType()
+                    .GetProperties(BindingFlags.Public | BindingFlags.Instance | BindingFlags.DeclaredOnly);
+
+                foreach (var property in properties)
+                    if (typeof(BaseRule).IsAssignableFrom(property.PropertyType))
+                    {
+                        var child = (BaseRule) property.GetValue(this);
+                        if (child != null)
+                            yield return child;
+                    }
+                    else if (typeof(TerminalNode).IsAssignableFrom(property.PropertyType))
+                    {
+                        var child = (TerminalNode) property.GetValue(this);
+                        if (child?.TokenRecord != null)
+                            yield return child;
+                    }
+                    else if (typeof(IEnumerable<BaseRule>).IsAssignableFrom(property.PropertyType))
+                    {
+                        var children = (IEnumerable<BaseRule>) property.GetValue(this);
+                        if (children == null) continue;
+                        foreach (var child in children)
+                            if (child != null)
+                                yield return child;
+                    }
+            }
+        }
     }
 }
