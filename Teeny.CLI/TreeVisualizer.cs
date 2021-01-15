fc47f42e530c9863f6ee9bc4bd13121f6a2edf43
Youssef Raafat <YoussefRaafatNasry@gmail.com>
2021-01-15 07:25:10 +0200

    Visualize Parse Tree without External Libraries

---
+++ Teeny.CLI/TreeVisualizer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using Teeny.Core.Parse;
+
+namespace Teeny.CLI
+{
+    static class TreeVisualizer
+    {
+        public static void Visualize(Node node, bool isRoot = true, string indent = "", bool isLast = true)
+        {
+            var marker = isLast ? "└───" : "├───";
+            Console.Write(indent);
+            Console.Write(isRoot ? "" : marker);
+            Console.Write(node.Name);
+            Console.WriteLine();
+
+            indent += isLast ? "    " : "│   ";
+
+            if (node.Children == null) return;
+
+            var last = node.Children.LastOrDefault();
+
+            foreach (var child in node.Children)
+                Visualize(child, false, isRoot ? "" : indent, child == last);
+        }
+    }
+}
